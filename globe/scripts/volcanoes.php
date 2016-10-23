6ab9c082a0798c40fb43b59023e542e42c423ed8
Ekansh Vinaik <ekansh@umd.edu>
2016-10-23 16:04:47 -0400

    Fix

--- globe/scripts/volcanoes.php
+++ globe/scripts/volcanoes.php
@@ -1,25 +1,4 @@
-<?php
-// $data = file_get_contents('https://www.ngdc.noaa.gov/nndc/struts/results?type_0=Exact&query_0=$HAZ_EVENT_ID&t=102557&s=50&d=54&dfn=volerup.txt');
-// $temp = explode("\n", $data);
-// $data = [];
-// for ($i = 1, $size = count($temp); $i < $size; $i++) {
-// 	if (!empty($temp[$i])) {
-// 		$erupt = explode("\t", $temp[$i]);
-// 		$lat = floatval($erupt[8]);
-// 		$lon = floatval($erupt[9]);
-// 		$deaths = 0.0;
-// 		$injuries = 0.0;
-// 		if (!empty($erupt[26]))
-// 			$deaths = floatval($erupt[26]);
-// 		if (!empty($erupt[30]))
-// 			$injuries = floatval($erupt[30]);
-// 		$data[] = $lat;
-// 		$data[] = $lon;
-// 		$data[] = $deaths + $injuries;
-// 	}
-// }
 
-// echo json_encode($data);
 <?php
 ini_set('display_errors', 1);
 require "twitteroauth/autoload.php";
