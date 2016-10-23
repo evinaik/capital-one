c374430c72cf14b69d9ff8878b01b26240986928
Ekansh Vinaik <ekansh@umd.edu>
2016-10-23 15:44:24 -0400

    Fix volcano

--- globe/index.php
+++ globe/index.php
@@ -101,8 +101,9 @@
       var globe = new DAT.Globe(container, {
         colorFn : new THREE.Color(1, 0, 0)
       });
-      var data = $.get('scripts/volcanoes.php').responseText;
-      globe.addData(data, {format: 'magnitude'});
+      var data = $.get('scripts/volcanoes.php');
+      console.log(data);
+      globe.addData(data.responseText, {format: 'magnitude'});
       globe.createPoints();
       globe.animate();
       document.body.style.backgroundImage = 'none';
