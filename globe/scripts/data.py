0f3539965859f7b01188cc8336e397fa54e98b22
Ekansh Vinaik <ekansh@umd.edu>
2016-10-24 17:25:29 -0400

    add final vars

--- globe/scripts/data.py
+++ globe/scripts/data.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 import twitter
 
-api = twitter.Api(consumer_key='VTiKXADVTl6WR1r0d3I28VD4y',
-                  consumer_secret='owYftIYKzxdYvSUEdEWkM88emP4SMay61AsXhEnroKRY8NlvsC',
-                  access_token_key='786003535733194752-ZA7oCmdpKxVuTWM9yPZGvKwmiRzpkZ9',
-                  access_token_secret='YZbwWkV6pRHEfO7dQ8HtOJFw5ZNdy3WV1uCp0r6Kkwma1')
+CONSUMER_KEY = 'VTiKXADVTl6WR1r0d3I28VD4y'
+CONSUMER_SECRET = 'owYftIYKzxdYvSUEdEWkM88emP4SMay61AsXhEnroKRY8NlvsC'
+ACCESS_KEY = '786003535733194752-ZA7oCmdpKxVuTWM9yPZGvKwmiRzpkZ9'
+ACCESS_SECRET = 'YZbwWkV6pRHEfO7dQ8HtOJFw5ZNdy3WV1uCp0r6Kkwma1'
+
+api = twitter.Api(consumer_key=CONSUMER_KEY,
+                  consumer_secret=CONSUMER_SECRET,
+                  access_token_key=ACCESS_KEY,
+                  access_token_secret=ACCESS_SECRET)
 results = api.GetSearch(raw_query="q=%3A)")
-print 'allah'
-# print results
+
+print results
