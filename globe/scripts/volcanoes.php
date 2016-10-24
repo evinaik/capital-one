2cf92f1a16670e05f76a0179a7dbd5057825139c
Ekansh Vinaik <ekansh@umd.edu>
2016-10-24 16:58:46 -0400

    test python

--- globe/scripts/volcanoes.php
+++ globe/scripts/volcanoes.php
@@ -1,17 +1,20 @@
 <?php
-ini_set('display_errors', 1);
-require "twitteroauth/autoload.php";
+// ini_set('display_errors', 1);
+// require "twitteroauth/autoload.php";
 
-use Abraham\TwitterOAuth\TwitterOAuth;
+// use Abraham\TwitterOAuth\TwitterOAuth;
 
-$ckey = 'VTiKXADVTl6WR1r0d3I28VD4y';
-$csec = 'owYftIYKzxdYvSUEdEWkM88emP4SMay61AsXhEnroKRY8NlvsC';
-$access_token = '786003535733194752-ZA7oCmdpKxVuTWM9yPZGvKwmiRzpkZ9';
-$access_token_secret = 'YZbwWkV6pRHEfO7dQ8HtOJFw5ZNdy3WV1uCp0r6Kkwma1';
+// $ckey = 'VTiKXADVTl6WR1r0d3I28VD4y';
+// $csec = 'owYftIYKzxdYvSUEdEWkM88emP4SMay61AsXhEnroKRY8NlvsC';
+// $access_token = '786003535733194752-ZA7oCmdpKxVuTWM9yPZGvKwmiRzpkZ9';
+// $access_token_secret = 'YZbwWkV6pRHEfO7dQ8HtOJFw5ZNdy3WV1uCp0r6Kkwma1';
 
-$connection = new TwitterOAuth($ckey, $csec, $access_token, $access_token_secret);
-$content = $connection->get("account/verify_credentials");
-$statuses = $connection->get("search/tweets", ["q" => "twitterapi", "lang" => "en"]);
+// $connection = new TwitterOAuth($ckey, $csec, $access_token, $access_token_secret);
+// $content = $connection->get("account/verify_credentials");
+// $statuses = $connection->get("search/tweets", ["q" => "twitterapi", "lang" => "en"]);
 
-echo json_encode($statuses);
+// echo json_encode($statuses);
+
+$output = shell_exec('/usr/bin/python scripts/test.py')
+echo $output
 ?>
--- scripts/test.py
+++ scripts/test.py
@@ -1,3 +1,1 @@
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return ["<h1 style='color:blue'>Hello There!</h1>"]
+print 'testing'
