3e443ed254f0009019b90121338beae02ff68e3f
Ekansh Vinaik <ekansh@umd.edu>
2016-10-24 17:15:37 -0400

    test code

--- globe/scripts/data.py
+++ globe/scripts/data.py
@@ -5,6 +5,6 @@ api = twitter.Api(consumer_key='VTiKXADVTl6WR1r0d3I28VD4y',
                   consumer_secret='owYftIYKzxdYvSUEdEWkM88emP4SMay61AsXhEnroKRY8NlvsC',
                   access_token_key='786003535733194752-ZA7oCmdpKxVuTWM9yPZGvKwmiRzpkZ9',
                   access_token_secret='YZbwWkV6pRHEfO7dQ8HtOJFw5ZNdy3WV1uCp0r6Kkwma1')
-results = api.GetSearch(
-    raw_query="q=%3A)")
+results = api.GetSearch(raw_query="q=%3A)")
 print results
+print 'allah'
