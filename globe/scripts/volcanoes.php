3b9e2f7c2c674d4b9880d6e9d0000e3f2428177c
Ekansh Vinaik <ekansh@umd.edu>
2016-10-24 16:11:26 -0400

    Fix bug

--- globe/scripts/volcanoes.php
+++ globe/scripts/volcanoes.php
@@ -7,7 +7,7 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 
 $ckey = 'VTiKXADVTl6WR1r0d3I28VD4y';
 $csec = 'owYftIYKzxdYvSUEdEWkM88emP4SMay61AsXhEnroKRY8NlvsC';
-$access_token = '86003535733194752-ZA7oCmdpKxVuTWM9yPZGvKwmiRzpkZ9';
+$access_token = '786003535733194752-ZA7oCmdpKxVuTWM9yPZGvKwmiRzpkZ9';
 $access_token_secret = 'YZbwWkV6pRHEfO7dQ8HtOJFw5ZNdy3WV1uCp0r6Kkwma1';
 
 $connection = new TwitterOAuth($ckey, $csec, $access_token, $access_token_secret);
