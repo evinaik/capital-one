0c80259b808d0a739a38d3a7291234143ce3c1d4
Ekansh Vinaik <ekansh@umd.edu>
2016-10-26 19:26:24 -0400

    exclude datetime from txt

--- scripts/data.py
+++ scripts/data.py
@@ -46,7 +46,8 @@ class TweetStreamer(TwythonStreamer):
             while len(self.allData) > 4 and (curr - self.allData[4]).total_seconds() >= 5:
                 self.allData = self.allData[5:]
             for i in self.allData:
-                temp += str(i) + ","
+                if not isinstance(i, datetime.datetime):
+                    temp += str(i) + ","
             f.write(temp)
 
 def call(streamer):
