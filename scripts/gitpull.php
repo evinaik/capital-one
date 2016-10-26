79c1a763c1926206428eb733bdbab3a1cf294000
Ekansh Vinaik <ekansh@umd.edu>
2016-10-25 21:13:41 -0400

    test

--- scripts/gitpull.php
+++ scripts/gitpull.php
@@ -1,4 +1,4 @@
 <?php
-$output = shell_exec('cd /var/www/capitalOne && /usr/bin/env git pull');
+$output = shell_exec('cd /var/www/capitalOne && /usr/bin/env git pull 2>&1 > hook-output.log');
 echo $output;
 ?>
