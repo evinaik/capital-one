55f6842af59e0b89008d685f8c5f3b1ec9535a8c
Ekansh Vinaik <ekansh@umd.edu>
2016-10-27 00:42:48 -0400

    change reset

--- globe.js
+++ globe.js
@@ -174,7 +174,7 @@
 
   function resetPoints() {
     var i;
-    for(i = scene.children.length - 2; i >= 0; i--){
+    while (scene.children.length > 2) {
      obj = scene.children[i + 2];
      scene.remove(obj);
    }
