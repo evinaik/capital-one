3b2c6a5b7954e9eda7083ebc93c9da5136a686b6
Ekansh Vinaik <ekansh@umd.edu>
2016-10-27 07:20:07 -0400

    reset timeout

--- scripts/data.py
+++ scripts/data.py
@@ -44,7 +44,7 @@ class TweetStreamer(TwythonStreamer):
         self.allData.extend([lat, lon, size/(size+5000.0), 1 if pos_score >= neg_score else 0, datetime.datetime.now()])
         temp = ''
         curr = datetime.datetime.now()
-        while len(self.allData) > 4 and (curr - self.allData[4]).total_seconds() >= 3:
+        while len(self.allData) > 4 and (curr - self.allData[4]).total_seconds() >= 600:
             self.allData = self.allData[5:]
         for i in self.allData:
             if not isinstance(i, datetime.datetime):
