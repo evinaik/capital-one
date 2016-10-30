44aa4186f7c5b54bc5334f171d0e09a889db7577
Ekansh Vinaik <ekansh@umd.edu>
2016-10-30 09:55:20 -0400

    change formula

--- js/globe.js
+++ js/globe.js
@@ -217,7 +217,7 @@
         lng = data[i + 1];
         size = data[i + 2];
         color = colorFn(size, data[i + 3]);
-        size = size * 40 + 10;
+        size = size * 40 + 50;
         addPoint(lat, lng, size, color, subgeo);
       }
       this._baseGeometry.morphTargets.push({'name': opts.name, vertices: subgeo.vertices});
